745749d255e6ee8521cadced0bc6b9edb9863e5a
Shalupandeys <147485683+Shalupandeys@users.noreply.github.com>
2025-04-11 15:44:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";   // treats all js code as newer version
+
+// alert(3+3)  //doesnt give output here as we are using nodejs,not browser(if we run this same thing in browser it gives output as 6) 
+//code readability must be maintained
+console.log(3
+    +
+    3)   //dont write code like this coz this is not a gud practice as code  readability shld be high
+console.log("hitesh")
+
+
+let name = "shalu"    //string datatype
+let age= 18           //number dt
+let isLoggedIn= false       //boolean dt
+
+
+//number dt range=> 2 to power 53
+//bigint
+//string => we can use "" or'' //preferred is ""
+//boolean=> true/false
+//null => it is a standalone value //null is representation of empty val
+//undefined => means wen val is not defined/assigned
+//symbol=> used wen we talk abt uniqueness
+//object
+
+console.log(typeof "shalu");     //o/p:string
+console.log(typeof null);        //o/p: object ****
+console.log(typeof undefined);   //o/p: undefined ****
